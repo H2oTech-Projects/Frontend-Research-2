91bf03993dbb14c337cc32b75f35290baea5c9d9
Saujan Maka <saujan@h2otechonline.com>
2025-02-11 13:09:03 +0545

    [RT-23] add css to popup message

--- src/utils/map.tsx
+++ src/utils/map.tsx
@@ -1,9 +1,8 @@
 export const buildPopupMessage = (properties: any) => {
   if (!properties) return "";
-  console.log(Object.keys(properties))
-  const tableContent = Object.keys(properties).map((key) => {return(`<tr><td>${key}</td><td>:${!!properties[key] ? properties[key] : ""}</td></tr>`)})
+  const tableContent = Object.keys(properties).map((key) => {return(`<tr class="py-10 border-b border-gray-200 hover:bg-gray-100"><td>${key}</td><td>:${!!properties[key] ? properties[key] : ""}</td></tr>`)})
   return (
-    `<table>
+    `<table class="table-fixed md:table-fixed">
       <tbody>
         ${tableContent}
       </tbody>
