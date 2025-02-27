5a3f934b1e40eaabb5bc7a9467347735a79f077d
jumirgosain <jumir@h2otechonline.com>
2025-02-27 17:09:24 +0545

    ci: :construction: [RT-16] working on form array

--- src/pages/management/water/field/AddField.tsx
+++ src/pages/management/water/field/AddField.tsx
@@ -1,27 +1,23 @@
 import PageHeader from '@/components/PageHeader'
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { useFieldArray, useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import dayjs from "dayjs";
+
 import {
   Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Calendar } from "@/components/ui/calendar";
+
 import { FormInput } from '@/components/FormComponent/FormInput'
 const formSchema = z.object({
   fieldID: z.string().min(5, "FieldID must be at least 5 characters"),
-  farmedAcres: z.coerce.number()
+  farmedAcres: z.coerce.number(),
+  coordinates: z.array(
+    z.object({
+      lat: z.coerce.number().min(-90, "Latitude must be between -90 and 90").max(90, "Latitude must be between -90 and 90"),
+      lng: z.coerce.number().min(-180, "Longitude must be between -180 and 180").max(180, "Longitude must be between -180 and 180"),
+    })
+  ),
 });
 
 type FormValues = z.infer<typeof formSchema>;
@@ -33,7 +29,17 @@ const AddField = () => {
     defaultValues: {
       fieldID: "",
       farmedAcres: undefined,
+      coordinates: [],
     },
+  });
+    const {
+    fields,
+    append,
+    prepend,
+    remove,
+  } = useFieldArray({
+    control: form.control,
+    name: "coordinates"
   });
  const onSubmit = (data: FormValues) => {
     console.log("Form Data:", data);
@@ -51,12 +57,15 @@ const AddField = () => {
         {/* Name Input */}
       <FormInput control={form.control} name='fieldID' label='FieldID' placeholder='Enter FieldID' type='text' />
       <FormInput control={form.control} name='farmedAcres' label='Farmed Acres' placeholder='Enter Farmed Acres' type='number' />
-
-    
-
-        {/* Accept Terms Checkbox */}
-     
-
+      {
+        fields.map((field, index) => (
+          <div key={field.id} className='flex gap-2'>
+              Test
+            <FormInput control={form.control} name={`coordinates[${index}].lat`} label='Latitude' placeholder='Enter Latitude' type='number' />
+            <FormInput control={form.control} name={`coordinates[${index}].lng`} label='Longitude' placeholder='Enter Longitude' type='number' />
+            <Button onClick={() => remove(index)}>Remove</Button>
+          </div>
+        ))    }  
         <Button type="submit">Submit</Button>
       </form>
     </Form>
