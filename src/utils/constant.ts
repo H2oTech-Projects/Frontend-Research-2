235c37322745fb39b379369feacc24001d0c4a8b
jumirgosain <jumir@h2otechonline.com>
2025-03-10 13:57:23 +0545

    feat: [RT-35] added column properties constant

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,32 @@
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
+export const  farmUnitColumnProperties = {
+    "account_id": "str",
+    "farm_unit_zone": "str",
+    "fu_total_alloc_af": "number",
+    "fu_sy_ac": "number",
+    "fu_tw_ac": "number",
+    "fu_alloc_af": "number",
+    "fu_carryover_af": "number",
+    "fu_total_adjustment_af": "number",
+    "fu_etaw_af": "number",
+    "fu_remain_af": "number",
+    "fu_remain_pct": "number",
+    "parcel_id": "str",
+    "zone_abr": "str",
+    "fu_etaw_pct": "number",
+    "parcels": "str",
+    "full_label": "str",
+    "category": "str"
+  };
+export const  parcelColumnProperties =  {
+    "parcel_id": "str",
+    "account_id": "str",
+    "zone_name": "str",
+    "zone_abr": "str",
+    "alloc_af": "str",
+    "primary_crop": "str",
+    "legal_ac": "number",
+    "carryover_af": "number",
+    "coords": "str"
+  };
