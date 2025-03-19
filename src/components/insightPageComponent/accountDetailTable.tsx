89893c7b36322011daf882a2f54c92948c6be92e
jumirgosain <jumir@h2otechonline.com>
2025-03-19 17:03:13 +0545

    feat: [RT-35] added new component for insight page

---
+++ src/components/insightPageComponent/accountDetailTable.tsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { AccountDetailType } from '@/types/apiResponseType';
+
+interface AccountDetailTableProps {
+  accountDetailLoading: boolean;
+  accountDetail: AccountDetailType;
+}
+
+const AccountDetailTable = ({ accountDetailLoading, accountDetail }: AccountDetailTableProps) => {
+  if (accountDetailLoading)
+    return <Table>
+      <TableHeader >
+        <TableRow >
+          <TableHead className="bg-royalBlue !text-slate-50 hover:bg-none text-left w-64">Description</TableHead>
+          <TableHead className="bg-royalBlue !text-slate-50 hover:bg-none text-left">Value</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell
+            colSpan={2}
+            className="h-auto text-center"
+          >
+            Loading...
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  return (
+    <Table>
+      <TableHeader >
+        <TableRow >
+          <TableHead className="bg-royalBlue !text-slate-50 hover:bg-none text-left w-64">Description</TableHead>
+          <TableHead className="bg-royalBlue !text-slate-50 hover:bg-none text-left">Value</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="!text-left">
+            Account ID:
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.account_id}
+          </TableCell>
+        </TableRow>
+
+        <TableRow >
+          <TableCell className="!text-left">
+            Account Name:
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.account_name}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="!text-left">
+            Mailing Address:
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.mailing_address}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="!text-left w-auto">
+            Start Date (YYYY-MM-DD):
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.start_date}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="!text-left w-auto">
+            End Date (YYYY-MM-DD):
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.end_date}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="!text-left">
+            Measurement Method:
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.msmt_method}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="!text-left">
+            Report Creation Date:
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.report_creation_date}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="!text-left">
+            Report Revision Date:
+          </TableCell>
+          <TableCell className="!text-left">
+            {accountDetail?.report_revision_date}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
+
+export default React.memo(AccountDetailTable)
---
+++ src/components/insightPageComponent/chartContainer.tsx
@@ -0,0 +1,32 @@
+import { AllocationChartDataType } from '@/types/apiResponseType'
+import React from 'react'
+import StackedBarChart from '../charts/stackedBarChart'
+
+const ChartContainer = ({ loading, data, setSelectedFarm }: { loading: boolean, data: AllocationChartDataType[], setSelectedFarm: Function }) => {
+  if (loading)
+    return (
+      <div
+        className={"dark:bg-slate-500 rounded-[8px] pb-[25px] my-2 shadow-[0px_19px_38px_rgba(0,0,0,0.3),0px_15px_12px_rgba(0,0,0,0.22)]"}
+        style={{ height: 150 }}
+      >
+        <div className="flex justify-center items-center h-full"> Data is Loading.. </div>
+      </div>
+    )
+  const ChartBars = () => data?.length > 0 ?
+    <StackedBarChart
+      data={data}
+      config={{ margin: { top: 20, right: 30, left: 40, bottom: 5 } }}
+      layout={'vertical'}
+      stack1={'remaining'}
+      stack2={'allocation_used'}
+      setSelectedFarm={setSelectedFarm}
+    /> : <div className="flex justify-center items-center h-full">No Data Available</div>
+  return <div
+    className={"dark:bg-slate-500 rounded-[8px] pb-[25px] my-2 shadow-[0px_19px_38px_rgba(0,0,0,0.3),0px_15px_12px_rgba(0,0,0,0.22)]"}
+    style={{ height: 70 * data?.length + 80 }}
+  >
+    <ChartBars />
+  </div>
+}
+
+export default React.memo(ChartContainer)
