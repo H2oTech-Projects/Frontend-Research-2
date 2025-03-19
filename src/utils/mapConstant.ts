5f6a208f982791cbc32ac1163ea25be2c0e160e0
jumirgosain <jumir@h2otechonline.com>
2025-03-19 17:04:43 +0545

    feat: [RT-35] created constant file to create constant for map

---
+++ src/utils/mapConstant.ts
@@ -0,0 +1,17 @@
+export const geoFarmJsonStyle = {
+  color: "#16599A", // Border color
+  fillColor: "red", // Fill color for normal areas
+  fillOpacity: 0.5,
+  weight: 2,
+};
+
+export const LeafletMapConfig = { 'minZoom': 4, 'containerStyle': { height: "100%", width: "100%", overflow: "hidden", borderRadius: "8px" } };
+
+export const geoJsonStyle = {
+  color: "#16599A", // Border color
+  fillColor: "lightblue", // Fill color for normal areas
+  fillOpacity: 0.5,
+  weight: 2,
+};
+
+export const InsightMapPosition = { center: [36.96830684650072, -120.26398612842706], polygon: [], fieldId: "", viewBound: [] };
