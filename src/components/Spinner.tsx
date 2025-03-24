7d1e558364b8ee3ae79fdc3cafdae10f874a4cfd
jumirgosain <jumir@h2otechonline.com>
2025-03-24 13:02:28 +0545

    feat: [RT-35] added spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import { Loader } from "lucide-react";
+
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Loader className="w-6 h-6 animate-spin text-blue-500" />
+    </div>
+  );
+};
+
+export default Spinner;
