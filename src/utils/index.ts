9f8948edda00cebcf40452f88f23da91c3eb2fd8
jumirgosain <jumir@h2otechonline.com>
2025-03-27 08:42:00 +0545

    feat: [RT-40] adding debounce function

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timer: NodeJS.Timeout | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+}
