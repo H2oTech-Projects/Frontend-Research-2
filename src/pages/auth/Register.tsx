0a8caa3cc28cb1514a2bd165dac05517121deb7c
jumirgosain <jumir@h2otechonline.com>
2025-03-31 17:40:26 +0545

    feat: [RT-41] added register page

---
+++ src/pages/auth/Register.tsx
@@ -0,0 +1,119 @@
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { cn } from "../../utils/cn";
+import AuthenticationCard from "../../components/AuthenticationCard";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+
+const Register = () => {
+  const isDesktopDevice = useMediaQuery("(min-width: 768px)");
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email address").required("Email is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      gender: "",
+      state: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("Form Submitted:", values);
+      const loginStatus = true;
+      sessionStorage.setItem("isLoggedIn", JSON.stringify(loginStatus));
+      toast.success("Registration successful!");
+    },
+  });
+
+  return (
+    <AuthenticationCard
+
+      children={
+        <div className={cn("flex flex-col items-center gap-4", isDesktopDevice ? "px-6 pb-4 " : "px-3 pt-5")}>
+          <h1 className="border-b border-solid border-royalBlue p-0.5 pt-0 text-2xl font-semibold text-royalBlue">Register</h1>
+         <form onSubmit={formik.handleSubmit} className="mt-2 space-y-3">
+                    <input
+                        type="text"
+                        name="firstName"
+                        placeholder="First Name"
+                        className="w-full rounded-lg border p-1 dark:bg-gray-700"
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <input
+                        type="text"
+                        name="lastName"
+                        placeholder="Last Name"
+                        className="w-full rounded-lg border p-1 dark:bg-gray-700"
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        className="w-full rounded-lg border p-1 dark:bg-gray-700"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email && (
+                        <div className="text-sm text-red-500">{formik.errors.email}</div>
+                    )}
+                    <select
+                        name="gender"
+                        className="w-full rounded-lg border p-1 dark:bg-gray-700 appearance-none"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        defaultValue=""
+                    >
+                        <option value="" disabled hidden>Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    {formik.touched.gender && formik.errors.gender && (
+                        <div className="text-sm text-red-500">{formik.errors.gender}</div>
+                    )}
+                    <select
+                        name="state"
+                        className="w-full rounded-lg border p-1 dark:bg-gray-700 appearance-none"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        
+                    >
+                        <option value="" disabled hidden>Select State</option>
+                        <option value="California">California</option>
+                        <option value="New York">New York</option>
+                        <option value="Texas">Texas</option>
+                    </select>
+                    {formik.touched.state && formik.errors.state && (
+                        <div className="text-sm text-red-500">{formik.errors.state}</div>
+                    )}
+                    <button
+                        type="submit"
+                        className="w-full rounded-lg bg-blue-500 p-1 text-white hover:bg-blue-600"
+                    >
+                        Register
+                    </button>
+                    <Link
+                        to="/auth/login"
+                        className="mt-2 flex items-center justify-center text-blue-500 hover:underline"
+                    >
+                        <ArrowLeft size={15} /> Back
+                    </Link>
+                </form>
+        </div>
+      }
+    />
+  );
+};
+
+export default Register;
