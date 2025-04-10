b40f4671bf78330d8c1a3257d307ae0dff923f84
jumirgosain <jumir@h2otechonline.com>
2025-04-10 13:44:57 +0545

    feat: [RT-41] integrated register api call and changed css

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -6,68 +6,74 @@ import AuthenticationCard from "../../components/AuthenticationCard";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
+import { usePostRegisterUser } from "@/services/registration";
 
 const Register = () => {
   const isDesktopDevice = useMediaQuery("(min-width: 768px)");
   const validationSchema = Yup.object().shape({
     email: Yup.string().email("Invalid email address").required("Email is required"),
   });
-
+  const { mutate, isPending, isError, error, isSuccess, data } = usePostRegisterUser();
   const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
       email: "",
-      gender: "",
-      state: "",
     },
     validationSchema,
     onSubmit: (values) => {
       console.log("Form Submitted:", values);
-      const loginStatus = true;
-      sessionStorage.setItem("isLoggedIn", JSON.stringify(loginStatus));
-      toast.success("Registration successful!");
+      mutate({
+        email: values.email,
+        username: values.firstName + values.lastName,
+        password1: "10passworduser",
+        password2: "10passworduser",
+        first_name: values.firstName,
+        last_name: values.lastName,
+      });
     },
   });
+
+
 
   return (
     <AuthenticationCard
 
       children={
-        <div className={cn("flex flex-col items-center gap-4", isDesktopDevice ? "px-6 pb-4 " : "px-3 pt-5")}>
+        <div className={cn("flex flex-col items-center gap-4", isDesktopDevice ? "px-6 py-7 " : "px-3 pt-5")}>
           <h1 className="border-b border-solid border-royalBlue p-0.5 pt-0 text-2xl font-semibold text-royalBlue">Register</h1>
-         <form onSubmit={formik.handleSubmit} className="mt-2 space-y-3">
-                    <input
-                        type="text"
-                        name="firstName"
-                        placeholder="First Name"
-                        className="w-full rounded-lg border p-1 dark:bg-gray-700"
-                        value={formik.values.firstName}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                    />
-                    <input
-                        type="text"
-                        name="lastName"
-                        placeholder="Last Name"
-                        className="w-full rounded-lg border p-1 dark:bg-gray-700"
-                        value={formik.values.lastName}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                    />
-                    <input
-                        type="text"
-                        name="email"
-                        placeholder="Email"
-                        className="w-full rounded-lg border p-1 dark:bg-gray-700"
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                    />
-                    {formik.touched.email && formik.errors.email && (
-                        <div className="text-sm text-red-500">{formik.errors.email}</div>
-                    )}
-                    <select
+          <form onSubmit={formik.handleSubmit} className="mt-2 space-y-3 ">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              className="w-full rounded-lg border p-1 dark:bg-gray-700"
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              className="w-full rounded-lg border p-1 dark:bg-gray-700"
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="w-full rounded-lg border p-1 dark:bg-gray-700"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <div className="text-sm text-red-500">{formik.errors.email}</div>
+            )}
+            {/* <select
                         name="gender"
                         className="w-full rounded-lg border p-1 dark:bg-gray-700 appearance-none"
                         onChange={formik.handleChange}
@@ -96,20 +102,23 @@ const Register = () => {
                     </select>
                     {formik.touched.state && formik.errors.state && (
                         <div className="text-sm text-red-500">{formik.errors.state}</div>
-                    )}
-                    <button
-                        type="submit"
-                        className="w-full rounded-lg bg-blue-500 p-1 text-white hover:bg-blue-600"
-                    >
-                        Register
-                    </button>
-                    <Link
-                        to="/auth/login"
-                        className="mt-2 flex items-center justify-center text-blue-500 hover:underline"
-                    >
-                        <ArrowLeft size={15} /> Back
-                    </Link>
-                </form>
+                    )} */}
+            <div className="flex w-full flex items-center justify-center">
+              <button
+                type="submit"
+                className="w-1/2  rounded-lg bg-blue-500 p-1 text-white hover:bg-blue-600"
+                disabled={isPending}
+              >
+                Register
+              </button>
+            </div>
+            <Link
+              to="/auth/login"
+              className="mt-2 flex items-center justify-center text-blue-500 hover:underline"
+            >
+              <ArrowLeft size={15} /> Back
+            </Link>
+          </form>
         </div>
       }
     />
