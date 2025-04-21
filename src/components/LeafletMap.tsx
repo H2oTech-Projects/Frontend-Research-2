3f63f78c5491e51e836f6fb8bb111a0d35444f24
Saujan Maka <saujan@h2otechonline.com>
2025-04-21 16:57:41 +0545

    [RT-35] add opacity slider and legends

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -3,6 +3,9 @@ import CustomZoomControl from "./MapController";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Layer } from "recharts";
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
+import "./sliderDesign.css"
 
 type mapConfiguration = {
     minZoom: number;
@@ -27,9 +30,34 @@ type LeafletMapTypes = {
     viewBound?: any;
 };
 const geoserverUrl = "https://staging.flowgeos.wateraccounts.com/geoserver/rt_2023/wms";
+const sld =`<StyledLayerDescriptor version="1.0.0">
+  <NamedLayer>
+    <Name>rt_2023:wy2023_202309_eta_accumulation_in</Name> <!-- Replace with actual layer name -->
+    <UserStyle>
+      <FeatureTypeStyle>
+        <Rule>
+          <RasterSymbolizer>
+            <ColorMap type="ramp">
+              <ColorMapEntry quantity="0.82677167654036998" label="0.8268" color="#9e6212"/>
+              <ColorMapEntry quantity="9.1027560192347003" label="9.1028" color="#bfa22d"/>
+              <ColorMapEntry quantity="17.378740361929033" label="17.3787" color="#d7db47"/>
+              <ColorMapEntry quantity="25.654724704623366" label="25.6547" color="#86c456"/>
+              <ColorMapEntry quantity="33.930709047317691" label="33.9307" color="#44b26b"/>
+              <ColorMapEntry quantity="42.206693390012028" label="42.2067" color="#4dc2a3"/>
+              <ColorMapEntry quantity="50.482677732706357" label="50.4827" color="#50bdc1"/>
+              <ColorMapEntry quantity="58.122047895193433" label="58.1220" color="#3b788f"/>
+              <ColorMapEntry quantity="64.488189697265994" label="64.4882" color="#2a3f65"/>
+            </ColorMap>
+          </RasterSymbolizer>
+        </Rule>
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>`
 const LeafletMap = ({ zoom, position, collapse, clickedField = null, viewBound, configurations = {'minZoom': 11, 'containerStyle': {}, enableLayers: false}, children }: LeafletMapTypes) => {
   const { center } = position;
   const [addedLayers, setAddedLayers] = useState(['rt_2023:wy2023_202309_eta_accumulation_in'])
+  const [opacity, setOpacity] = useState(1)
   const isMenuCollapsed = useSelector((state: any) => state.sideMenuCollapse.sideMenuCollapse)
   const MapHandler = () => {
     const map = useMap();
@@ -82,10 +110,108 @@ const LeafletMap = ({ zoom, position, collapse, clickedField = null, viewBound, 
     const addLegends = () => {
       if (addedLayers.length <= 0) return ;
       const url = `${geoserverUrl}?service=WMS&request=GetLegendGraphic&format=image/png&layer=${addedLayers[addedLayers.length-1]}`;
-      return (
-        <div className="absolute top-20 right-2 z-[1002] h-auto  w-auto p-2 rounded-[8px] bg-royalBlue text-slate-50 bg-opacity-65">
+      if (addedLayers[addedLayers.length-1] != 'rt_2023:wy2023_202309_eta_accumulation_in') {
+        return (
+          <div className="absolute top-20 right-2 z-[1002] h-auto  w-auto p-2 rounded-[8px] bg-royalBlue text-slate-50 bg-opacity-65">
           <label className="text-center">{layerMapper[addedLayers[addedLayers.length-1]].id}</label>
           <img src={url} alt="Legend" style={{ width: "80px", height: "150px" }} />
+          </div>
+        )
+      }
+      return (
+        <div className="flex flex-col absolute top-20 right-2 z-[1002] h-auto  w-[100px] p-2 m-2 rounded-[8px] bg-black text-slate-50">
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>0.8</span>
+            <i style={{
+            background: "#9e6212",
+            height: "17px",
+            width: "17px",
+            }}
+            ></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>9.1</span>
+            <i style={{
+              background: "#bfa22d",
+              height: "17px",
+              width: "17px",
+            }}></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>17.3</span>
+            <i style={{
+            background: "#d7db47",
+            height: "17px",
+            width: "17px",
+            }}
+            ></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>25.6</span>
+            <i style={{
+              background: "#86c456",
+              height: "17px",
+              width: "17px",
+            }}></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>33.9</span>
+            <i style={{
+            background: "#44b26b",
+            height: "17px",
+            width: "17px",
+            }}
+            ></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>42.2</span>
+            <i style={{
+              background: "#4dc2a3",
+              height: "17px",
+              width: "17px",
+            }}></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>50.4</span>
+            <i style={{
+              background: "#50bdc1",
+              height: "17px",
+              width: "17px",
+            }}></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>58.1</span>
+            <i style={{
+              background: "#3b788f",
+              height: "17px",
+              width: "17px",
+            }}></i>
+          </div>
+          <div className="flex flex-row pb-1">
+            <span style={{position: 'absolute',display: 'block',left: '35px'}}>64.4</span>
+            <i style={{
+              background: "#2a3f65",
+              height: "17px",
+              width: "17px",
+            }}></i>
+          </div>
+        </div>
+      )
+    }
+
+    const addSlider = () => {
+      return (
+        <div
+          className="flex flex-col absolute bottom-[2rem] right-[-5rem] z-[1002] h-auto  w-[500px] p-2 m-2 rounded-[8px] bg-green text-slate-50">
+          <Nouislider
+            connect= {[true, false]}
+            start={100}
+            tooltips= {true}
+            range= {{ min: 0, max: 100 }}
+            pips= {{ mode: 'steps'}}
+            step= {20}
+            onUpdate={(num) => {setOpacity(parseFloat(num[0])/100)}}
+          />
         </div>
       )
     }
@@ -96,6 +222,7 @@ const LeafletMap = ({ zoom, position, collapse, clickedField = null, viewBound, 
           <LayersControl.Overlay checked name="Evapotransiration (ET)">
             <WMSTileLayer
               url={`${geoserverUrl}`}
+              opacity= {opacity}
               params={{
                 format:"image/png",
                 layers:"rt_2023:wy2023_202309_eta_accumulation_in",
@@ -107,76 +234,42 @@ const LeafletMap = ({ zoom, position, collapse, clickedField = null, viewBound, 
           <LayersControl.Overlay name="Evapotranspiration of Applied Water (ETAW)">
             <WMSTileLayer
               url={`${geoserverUrl}`}
+              opacity= {opacity}
               params={{
                 format:"image/png",
                 layers:"rt_2023:wy2023_202309_etaw_accumulation_in",
                 transparent: true,
-                ...( { sld_body: sld } as Record<string, any> ),
+                //...( { sld_body: sld } as Record<string, any> ),
               }}
             />
           </LayersControl.Overlay>
           <LayersControl.Overlay name="Evapotranispiration of Percipitation (ETPR)">
             <WMSTileLayer
               url={`${geoserverUrl}`}
+              opacity= {opacity}
               params={{
                 format:"image/png",
                 layers:"rt_2023:wy2023_202309_etpr_accumulation_in",
                 transparent: true,
-                ...( { sld_body: sld } as Record<string, any> ),
+                //...( { sld_body: sld } as Record<string, any> ),
               }}
             />
           </LayersControl.Overlay>
           <LayersControl.Overlay name="Precipitation (P)">
             <WMSTileLayer
               url={`${geoserverUrl}`}
+              opacity= {opacity}
               params={{
                 format:"image/png",
                 layers:"rt_2023:wy2023_p_total_in",
                 transparent: true,
-                ...( { sld_body: sld } as Record<string, any> ),
+                // ...( { SLD_BODY: sld } as Record<string, any> ),
               }}
             />
           </LayersControl.Overlay>
         </>
       )
     }
-    //const sld = `<StyledLayerDescriptor version="1.0.0" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"> <NamedLayer> <Name>rt_2023:gg</Name> <UserStyle> <Title>A azure polygon style</Title> <FeatureTypeStyle> <Rule> <Title>azure polygon</Title> <PolygonSymbolizer> <Fill> <CssParameter name="fill">#0033cc </CssParameter> </Fill> <Stroke> <CssParameter name="stroke">#000000</CssParameter> <CssParameter name="stroke-width">0.5</CssParameter> </Stroke> </PolygonSymbolizer> </Rule> </FeatureTypeStyle> </UserStyle> </NamedLayer></StyledLayerDescriptor>`
-    const sld = `<?xml version="1.0" encoding="UTF-8"?>
-    <StyledLayerDescriptor version="1.0.0"
-     xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd"
-     xmlns="http://www.opengis.net/sld"
-     xmlns:ogc="http://www.opengis.net/ogc"
-     xmlns:xlink="http://www.w3.org/1999/xlink"
-     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <!-- a Named Layer is the basic building block of an SLD document -->
-      <NamedLayer>
-        <Name>rt_2023:default_polygon</Name>
-        <UserStyle>
-        <!-- Styles can have names, titles and abstracts -->
-          <Title>Default Polygon</Title>
-          <Abstract>A sample style that draws a polygon</Abstract>
-          <!-- FeatureTypeStyles describe how to render different features -->
-          <!-- A FeatureTypeStyle for rendering polygons -->
-          <FeatureTypeStyle>
-            <Rule>
-              <Name>rule1</Name>
-              <Title>Gray Polygon with Black Outline</Title>
-              <Abstract>A polygon with a gray fill and a 1 pixel black outline</Abstract>
-              <PolygonSymbolizer>
-                <Fill>
-                  <CssParameter name="fill">#AAAAAA</CssParameter>
-                </Fill>
-                <Stroke>
-                  <CssParameter name="stroke">#000000</CssParameter>
-                  <CssParameter name="stroke-width">1</CssParameter>
-                </Stroke>
-              </PolygonSymbolizer>
-            </Rule>
-          </FeatureTypeStyle>
-        </UserStyle>
-      </NamedLayer>
-    </StyledLayerDescriptor>
-    `
 
     return (
         <MapContainer
@@ -187,6 +280,7 @@ const LeafletMap = ({ zoom, position, collapse, clickedField = null, viewBound, 
             minZoom={2}
             style={configurations.containerStyle || { height: "100%", width: "100%", overflow: "hidden", borderRadius: "8px" }}
         >
+            { configurations.enableLayers && addSlider() }
             <LayersControl position="bottomleft">
 
                 <LayersControl.BaseLayer
