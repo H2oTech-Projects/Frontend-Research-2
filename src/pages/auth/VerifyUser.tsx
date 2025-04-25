4aab3df34e362e945b752dc0aad62a3fcf25256d
jumirgosain <jumir@h2otechonline.com>
2025-04-25 14:58:52 +0545

    refactor: [RT-41] refactor api call function

--- src/pages/auth/VerifyUser.tsx
+++ src/pages/auth/VerifyUser.tsx
@@ -3,18 +3,25 @@ import Spinner from "@/components/Spinner";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { usePostCheckToken } from '@/services/registration';
 import { toast } from 'react-toastify';
-
 const VerifyUser = () => {
-  const { mutate, isPending, isError, error, isSuccess, data } = usePostCheckToken();
+  const { mutate } = usePostCheckToken();
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const userId = searchParams.get("user_id");
   const timestamp = searchParams.get("timestamp");
   const signature = searchParams.get("signature");
-
   useEffect(() => {
     if (userId && timestamp && signature) {
-      mutate({ user_id: userId, timestamp, signature });
+      mutate({ user_id: userId, timestamp, signature },
+        {
+          onSuccess: () => {
+            toast.success("User verified successfully! Please login to your account.");
+            navigate("/auth/login");
+          }, onError: (err) => {
+            toast.error("User verification failed. Please try again.");
+            navigate("/auth/register")
+          }
+        });
     } else {
       toast.error("Invalid verification link. Please try again.");
       navigate("/page-not-found")
@@ -22,33 +29,14 @@ const VerifyUser = () => {
 
   }, [userId, timestamp, signature]);
 
-  useEffect(() => {
-    if (data) {
-      if (isSuccess) {
-        navigate("/auth/login");
-        toast.success("User verified successfully! Please login to your account.");
 
-      } else {
-        navigate("/auth/register");
-        toast.error("User verification failed. Please try again.");
-      }
-    }
-  }, [data])
-
-    useEffect(() => {
-      if (error) {
-        navigate("/auth/register");
-        toast.error("User verification failed. Please try again.");
-      }
-    }, [isError])
-
-    return (
-      <div className="flex h-screen items-center justify-center dark:bg-slate-900 dark:text-white">
-        <div className='flex gap-3 justify-center items-start'>
-          <span>Please wait while verifying user </span> <Spinner />
-        </div>
+  return (
+    <div className="flex h-screen items-center justify-center dark:bg-slate-900 dark:text-white">
+      <div className='flex gap-3 justify-center items-start'>
+        <span>Please wait while verifying user </span> <Spinner />
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  export default VerifyUser
+export default VerifyUser
