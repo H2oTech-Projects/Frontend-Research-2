711d63e9bfd699be150c792b3ce655ae3e2bb44e
jumirgosain <jumir@h2otechonline.com>
2025-04-25 14:53:49 +0545

    fix: [RT-41] fixing key warning

--- src/utils/tools.tsx
+++ src/utils/tools.tsx
@@ -4,13 +4,13 @@ export const showErrorToast = (messages: any) => {
   let contents: any = [];
   Object.keys(messages).forEach(key => {
     messages[key].forEach((message: string) => {
-      contents.push(<li><b>{key}</b>: {message}</li>);
+      contents.push(<ul key={key} className="list-disc marker:text-sky-400"><li ><b>{key}</b>: {message}</li></ul>);
     });
   });
 
   toast.error(
     <div>
-      <ul className="list-disc marker:text-sky-400">{contents}</ul>
+      {contents}
     </div>
   );
 };
