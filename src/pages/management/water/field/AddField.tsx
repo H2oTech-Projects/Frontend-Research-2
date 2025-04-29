0194ca76b7cbebd554e7e4eb6083d8e31c7350dd
jumirgosain <jumir@h2otechonline.com>
2025-04-29 14:54:41 +0545

    feat: [RT-45] added field's form

--- src/pages/management/water/field/AddField.tsx
+++ src/pages/management/water/field/AddField.tsx
@@ -13,24 +13,31 @@ import "leaflet-draw/dist/leaflet.draw.css"
 import { LatLng, LeafletEvent, Layer,FeatureGroup as LeafletFeatureGroup  } from "leaflet"
 import FormCoordinatesMap from '@/components/FormComponent/FormCoordinatesMap'
 import { useRef } from 'react'
+import { FormTextbox } from '@/components/FormComponent/FormTextbox'
+import { FormRadioGroup } from '@/components/FormComponent/FormRadio'
 
 // ✅ Updated Schema: Coordinates as an array of [lat, lng]
 const formSchema = z.object({
   fieldName: z.string().min(5, "Field Name must be at least 5 characters"),
-  farmedAcres: z.coerce.number(),
-  year: z.string(),
+  field_desc: z.string().optional(),
+  field_cmnt: z.string().optional(),
+  IrrigAcres: z.coerce.number(),
+  StandbyAcres: z.coerce.number().optional(),
+  ActiveFlag: z.string().optional(),
+  MsmtMethod: z.string().min(1, "Measurement Method is required"),
+  MsmtMethodDesc: z.string().optional(),
   coordinates: z.array(z.array(
     z.tuple([
       z.coerce.number().min(-90, "Latitude must be between -90 and 90").max(90, "Latitude must be between -90 and 90"),
       z.coerce.number().min(-180, "Longitude must be between -180 and 180").max(180, "Longitude must be between -180 and 180"),
     ])
   )).min(1, "At least  coordinate is required").max(1, "Only one polygon is allowed"),
-  markers:  z.array(
-    z.tuple([
-      z.coerce.number().min(-90, "Latitude must be between -90 and 90").max(90, "Latitude must be between -90 and 90"),
-      z.coerce.number().min(-180, "Longitude must be between -180 and 180").max(180, "Longitude must be between -180 and 180"),
-    ])
-  ).min(1, "At least  coordinate is required"),
+  // markers:  z.array(
+  //   z.tuple([
+  //     z.coerce.number().min(-90, "Latitude must be between -90 and 90").max(90, "Latitude must be between -90 and 90"),
+  //     z.coerce.number().min(-180, "Longitude must be between -180 and 180").max(180, "Longitude must be between -180 and 180"),
+  //   ])
+  // ).min(1, "At least  coordinate is required"),
 });
 
 type FormValues = z.infer<typeof formSchema>;
@@ -42,17 +49,22 @@ const AddField = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       fieldName: "",
-      year: "",
-      farmedAcres: Number(''),
+      IrrigAcres: Number(''),
+      StandbyAcres: Number(''),
+      ActiveFlag: "true",
+      MsmtMethod: "",
+      MsmtMethodDesc: "",
+      field_desc: "",
+      field_cmnt: "",
       coordinates: [],
-      markers: [],
+      // markers: [],
     },
   });
   const polygons = form.watch("coordinates");
-  const { fields, append, remove } = useFieldArray({
-    control: form.control,
-    name: "coordinates",
-  });
+  // const { fields, append, remove } = useFieldArray({
+  //   control: form.control,
+  //   name: "coordinates",
+  // });
 
   // Helper function to extract coordinates from a polygon layer
   const getPolygonCoordinates = (layer: Layer): [number, number][] => {
@@ -94,34 +106,34 @@ const onPolygonDeleted = (e: LeafletEvent) => {
   };
 
   // Handle marker creation event
-  const onMarkerCreated = (e: LeafletEvent) => {
-    const layer = (e as any).layer;
-    const newMarker = getMarkerCoordinates(layer);
+  // const onMarkerCreated = (e: LeafletEvent) => {
+  //   const layer = (e as any).layer;
+  //   const newMarker = getMarkerCoordinates(layer);
 
-    // Add new marker to the list
-    form.setValue("markers",  [newMarker]);
-    form.clearErrors("markers"); // Clear errors on new marker creation
-  };
+  //   // Add new marker to the list
+  //   form.setValue("markers",  [newMarker]);
+  //   form.clearErrors("markers"); // Clear errors on new marker creation
+  // };
 
   // Handle marker edit event
-  const onMarkerEdited = (e: LeafletEvent) => {
-    const layers = (e as any).layers;
-    const updatedMarkers: [number, number][] = [];
+  // const onMarkerEdited = (e: LeafletEvent) => {
+  //   const layers = (e as any).layers;
+  //   const updatedMarkers: [number, number][] = [];
 
-    layers.eachLayer((layer: Layer) => {
-      updatedMarkers.push(getMarkerCoordinates(layer));
-    });
+  //   layers.eachLayer((layer: Layer) => {
+  //     updatedMarkers.push(getMarkerCoordinates(layer));
+  //   });
 
-    form.setValue("markers", updatedMarkers);
-  };
+  //   form.setValue("markers", updatedMarkers);
+  // };
 
   // Handle marker deletion event
-   const onMarkerDeleted = (e: LeafletEvent) => {
-    const layers = (e as any).layers;
-    if (layers.getLayers().length !== 0) {
-      form.setValue("markers", []); // Clear markers on delete
-    }
-  };
+  //  const onMarkerDeleted = (e: LeafletEvent) => {
+  //   const layers = (e as any).layers;
+  //   if (layers.getLayers().length !== 0) {
+  //     form.setValue("markers", []); // Clear markers on delete
+  //   }
+  // };
 
   const onSubmit = (data: FormValues) => {
     console.log("Form Data:", data);
@@ -135,13 +147,25 @@ const onPolygonDeleted = (e: LeafletEvent) => {
       />
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='bg-white rounded-lg shadow-md p-5 mt-3 h-[calc(100vh-132px)] flex flex-col gap-4'>
-         
+        <form onSubmit={form.handleSubmit(onSubmit)} className='bg-white rounded-lg shadow-md p-5 mt-3 h-auto flex flex-col gap-4'>
+         <div className='grid grid-cols-2 gap-4 mb-4'>
+          <div>
           <FormInput control={form.control} name='fieldName' label='Field Name' placeholder='Enter Field Name' type='text' />
-          <FormInput control={form.control} name='farmedAcres' label='Farmed Acres' placeholder='Enter Farmed Acres' type='number' />
-          <FormComboBox control={form.control} name = "year" label= 'Year' options={[{ label:"2022",value:"2022"},{ label:"2023",value:"2023"},{ label:"2024",value:"2024"}]}/>
-          <FormCoordinatesMap control={form.control} name="coordinates" label="Polygon Coordinates" onCreated={onPolygonCreated} onEdited={onPolygonEdited} onDeleted={onPolygonDeleted} type="polygon" refLayer={featureGroupPolygonRef} layerCounts='single'/>
-          <FormCoordinatesMap control={form.control} name="markers" label="Point Coordinates" onCreated={onMarkerCreated} onEdited={onMarkerEdited} onDeleted={onMarkerDeleted} type="marker" refLayer={featureGroupMarkerRef}/>
+          <FormInput control={form.control} name='IrrigAcres' label='Irrigable Acres' placeholder='Enter Irrigable  Acres' type='number' />
+          <FormInput control={form.control} name='StandbyAcres' label='Stand By Acres' placeholder='Enter Stand By  Acres' type='number' />
+          <FormTextbox control={form.control} name='field_desc' label='Field Description' placeholder='Enter Field Description'  />
+          <FormTextbox control={form.control} name='field_cmnt' label='Comments' placeholder='Comments...'  />
+          <FormRadioGroup control={form.control} name='ActiveFlag' label='Active status' options={[{ label: "Yes", value: "true" }, { label: "No", value: "false" }]} />
+          <FormComboBox control={form.control} name='MsmtMethod' label='Measurement Method' options={[{ label: "GPS", value: "GPS" }, { label: "Survey", value: "Survey" }, { label: "Aerial", value: "Aerial" }]} />
+          <FormTextbox control={form.control} name='MsmtMethodDesc' label='Measurement Method Description' placeholder='Enter Measurement Method Description'  />
+ {/* <FormComboBox control={form.control} name = "year" label= 'Year' options={[{ label:"2022",value:"2022"},{ label:"2023",value:"2023"},{ label:"2024",value:"2024"}]}/> */}      
+          </div>
+          <div> 
+             <FormCoordinatesMap control={form.control} name="coordinates" label="Polygon Coordinates" onCreated={onPolygonCreated} onEdited={onPolygonEdited} onDeleted={onPolygonDeleted} type="polygon" refLayer={featureGroupPolygonRef} layerCounts='single'/>
+          </div>
+         </div>
+         
+          {/* <FormCoordinatesMap control={form.control} name="markers" label="Point Coordinates" onCreated={onMarkerCreated} onEdited={onMarkerEdited} onDeleted={onMarkerDeleted} type="marker" refLayer={featureGroupMarkerRef}/> */}
           <Button className='mt-4 w-24' type="submit">Submit</Button>
         </form>
       </Form>
