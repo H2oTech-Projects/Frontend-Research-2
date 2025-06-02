af14fc27c1e94f14a43254b00dc7c5aea2854f86
jumirgosain <jumir@h2otechonline.com>
2025-06-02 13:47:49 +0545

    refactor: [RT-45] updated code for page header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Slash } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 
 interface PathType {
@@ -13,6 +14,7 @@ type PageHeaderProps = {
 };
 
 const PageHeader = ({ breadcrumbPathList, pageHeaderTitle }: PageHeaderProps) => {
+    const navigate = useNavigate();
     return (
         <div className="flex flex-col gap-1">
             <Breadcrumb>
@@ -21,7 +23,7 @@ const PageHeader = ({ breadcrumbPathList, pageHeaderTitle }: PageHeaderProps) =>
                         return (
                             <Fragment key={breadcrumbPath?.menuName}>
                                 <BreadcrumbItem>
-                                    <BreadcrumbLink href={`${breadcrumbPath?.menuPath}`}>{breadcrumbPath?.menuName}</BreadcrumbLink>
+                                    <BreadcrumbLink className="hover:cursor-pointer" onClick={()=>{navigate(breadcrumbPath?.menuPath)}}>{breadcrumbPath?.menuName}</BreadcrumbLink>
                                 </BreadcrumbItem>
                                 <BreadcrumbSeparator>/</BreadcrumbSeparator>
                             </Fragment>
