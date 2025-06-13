fcdde976f4c8d1af4121940780190b5c5348165c
jumirgosain <jumir@h2otechonline.com>
2025-06-13 16:34:38 +0545

    feat: [RT-54] integrated create api

--- src/pages/management/water/district/District.tsx
+++ src/pages/management/water/district/District.tsx
@@ -11,20 +11,26 @@ import { FormDatePicker } from "@/components/FormComponent/FormDatePicker";
 import { FormInput } from "@/components/FormComponent/FormInput";
 import { FormComboBox } from "@/components/FormComponent/FormRTSelect";
 import dayjs from "dayjs";
+import { useGetWaptOptions, useGetWaysOptions, usePostWays } from "@/services/timeSeries";
+import { convertKeysToSnakeCase } from "@/utils/stringConversion";
+import { toast } from "react-toastify";
+import { showErrorToast } from "@/utils/tools";
 const data = [{ name: "jumir", id: 12 }, { name: "jumir22", id: 21 }, { name: "jumir33", id: 32 }, { name: "jumir44", id: 24 }, { name: "jumir55", id: 54 }, { name: "jumir66", id: 68 }, { name: "jumir gosain", id: 645 }, { name: "jumir88", id: 68676 }, { name: "jumir77", id: 688 }, { name: "jumir99", id: 988 }]
 
-export const NestedChildArray: React.FC<{ form: any; listIndex: number, parentName: string ,control:any }> = ({ form, listIndex, parentName,control }) => {
+export const NestedChildArray: React.FC<{ form: any; listIndex: number, parentName: string ,control:any,yearList:any[] }> = ({ form, listIndex, parentName,control,yearList }) => {
   const { fields, append, remove } = useFieldArray({
     control,
     name: `wapList.${listIndex}.child`,
   });
+  const startEndDate = yearList?.filter((item:any) => item.value === form.watch("wayYear") )
+
   const formatNumber = (num: number) => num.toString().padStart(2, '0');
   return <div className="flex flex-col gap-2 items-center w-full">
     {fields?.map((item: any, index) => {
       return (
         <div key={index} className="flex flex-col w-full">
           <div className="flex items-center justify-center w-full gap-3">
-            <FormInput name={`wapList.${listIndex}.child.${index}.name`} label="Name" type="text" control={control} placeholder="name" disabled={true} />
+            <FormInput name={`wapList.${listIndex}.child.${index}.periodName`} label="Name" type="text" control={control} placeholder="name" disabled={true} />
               <div onPointerDown={(e) => e.stopPropagation()}>
               <FormDatePicker
                 control={control}
@@ -32,8 +38,8 @@ export const NestedChildArray: React.FC<{ form: any; listIndex: number, parentNa
                 label='Start Date'
                 showDateIcon={true}
                 disabled={index === 0 ? false : form.watch(`wapList.${listIndex}.child[${index-1}].endDate`) ? false : true }
-                minDate={index ==0 ? new Date("2026-01-01") : new Date(form.watch(`wapList.${listIndex}.child[${index-1}].endDate`))}
-                maxDate={new Date("2026-12-31")}
+                minDate={index ==0 ? new Date(startEndDate[0]?.waStartDate ?? "") : new Date(form.watch(`wapList.${listIndex}.child[${index-1}].endDate`))}
+                maxDate={new Date(startEndDate[0]?.waEndDate ?? "")}
               />
             </div>
             <div onPointerDown={(e) => e.stopPropagation()}>
@@ -44,7 +50,7 @@ export const NestedChildArray: React.FC<{ form: any; listIndex: number, parentNa
                 showDateIcon={true}
                 disabled={form.watch(`wapList.${listIndex}.child.${index}.startDate`) ? false: true}
                 minDate={new Date(form.watch(`wapList.${listIndex}.child.${index}.startDate`) )}
-                maxDate={new Date("2026-12-31")}
+                maxDate={new Date(startEndDate[0]?.waEndDate ?? "")}
               />
             </div>
 
@@ -54,7 +60,7 @@ export const NestedChildArray: React.FC<{ form: any; listIndex: number, parentNa
       )
     })}
     <div className="flex  w-full justify-center gap-2">
-      <Button variant={'secondary'} type="button" onPointerDown={(e) => e.stopPropagation()} onClick={(e) => { append({ name: parentName + " " + formatNumber(fields.length + 1), startDate: undefined, endDate: undefined }) }}> <Plus /></Button>
+      <Button variant={'secondary'} type="button" onPointerDown={(e) => e.stopPropagation()} onClick={(e) => { append({ periodName: parentName + " " + formatNumber(fields.length + 1), startDate: undefined, endDate: undefined }) }}> <Plus /></Button>
       {fields.length > 1 && <Button variant={'destructive'} type="button" onPointerDown={(e) => e.stopPropagation()} onClick={(e) => { remove(fields.length - 1) }}> <Trash /></Button>}
     </div>
 
@@ -63,58 +69,32 @@ export const NestedChildArray: React.FC<{ form: any; listIndex: number, parentNa
 }
 
 const District = () => {
-  const [listA, setListA] = useState<any>(data)
+  const [listA, setListA] = useState<any>([])
   // const [listB, setListB] = useState<any>([]);
   const [selectedA, setSelectedA] = useState<any>([]);
   const [selectedB, setSelectedB] = useState<any>([]);
   const [openAccordion, setOpenAccordion] = useState<any>([])
+  const { data:wapTypeOptions,isLoading:isWapTypeOptionsLoading } = useGetWaptOptions()
+  const {data:waysOptions,isLoading:waysOptionsLoading} = useGetWaysOptions();
+  const {mutate} = usePostWays();
+
+  useEffect(()=>{
+    if(wapTypeOptions && !isWapTypeOptionsLoading){
+     setListA(wapTypeOptions?.data)
+}
+
+},[wapTypeOptions])
+
   const formatNumber = (num: number) => num.toString().padStart(2, '0');
-  const yearList = [
-    {
-      value: "2028",
-      label: "2028"
-    },
-    {
-      value: "2027",
-      label: "2027"
-    },
-    {
-      value: "2026",
-      label: "2026"
-    },
-    {
-      value: "2025",
-      label: "2025"
-    },
-    {
-      value: "2024",
-      label: "2024"
-    },
-    {
-      value: "2023",
-      label: "2023"
-    },
-    {
-      value: "2022",
-      label: "2022"
-    },
-    {
-      value: "2021",
-      label: "2021"
-    },
-    {
-      value: "2020",
-      label: "2020"
-    }
-  ]
+
   type WayFormType = {
 
     wayYear: string;
     wapList: {
-      wapId: number;
+      waptId: number;
       name: string;
       child: {
-        name: string;
+        periodName: string;
         startDate: string | undefined;
         endDate: string | undefined;
       }[]
@@ -126,7 +106,7 @@ const District = () => {
       wayYear: "",
       wapList: []
     },
-  });
+  }); 
 
   const { fields, move, append, replace } = useFieldArray({
     control: form.control,
@@ -134,14 +114,14 @@ const District = () => {
   });
 
   const handleRightShift = () => {
-    const remainingItemA = listA?.filter((item: any) => !selectedA.includes(item.id))
-    const transferItem = listA?.filter((item: any) => selectedA.includes(item.id))
+    const remainingItemA = listA?.filter((item: any) => !selectedA.includes(item.value))
+    const transferItem = listA?.filter((item: any) => selectedA.includes(item.value))
     const listForForm = transferItem?.map((item: any) => {
       return {
-        wapId: item?.id,
-        name: item?.name,
+        waptId: item?.value,
+        name: item?.label,
         child: [{
-          name: item.name + " " + formatNumber(1), startDate: undefined,
+          periodName: item?.label + " " + formatNumber(1), startDate: undefined,
           endDate: undefined
         }]
       }
@@ -153,9 +133,10 @@ const District = () => {
     setSelectedA([]);
   }
   const handleLeftShift = () => {
-    const remainingItemsFields = fields?.filter((item: any) => !selectedB.includes(item.wapId))
-    const transferItem = fields?.filter((item: any) => selectedB.includes(item.wapId))
-    setListA([...listA, ...transferItem]);
+    const remainingItemsFields = fields?.filter((item: any) => !selectedB.includes(item.waptId))
+    const transferItem = fields?.filter((item: any) => selectedB.includes(item.waptId))
+    const listForType = transferItem?.map((item:any)=> {return {value:item?.waptId, label:item?.name }})
+    setListA([...listA, ...listForType]);
     form.setValue("wapList", remainingItemsFields)
     setSelectedB([]);
   }
@@ -195,8 +176,8 @@ const District = () => {
 
     if (e.active.id !== e.over.id) {
       const list = form.getValues("wapList");
-      const oldIdx = list.findIndex((item) => item.wapId.toString() === e.active.id.toString());
-      const newIdx = list.findIndex((item) => item.wapId.toString() === e.over!.id.toString());
+      const oldIdx = list.findIndex((item) => item.waptId.toString() === e.active.id.toString());
+      const newIdx = list.findIndex((item) => item.waptId.toString() === e.over!.id.toString());
       if (oldIdx !== -1 && newIdx !== -1) {
         move(oldIdx, newIdx); // This reorders the form array
       }
@@ -205,20 +186,31 @@ const District = () => {
 
 
   const onSubmit = (data: WayFormType) => {
-  console.log(data,"here")
-    const formattedData: WayFormType = {
+    const formattedData: any = {
       ...data,
       wapList: data.wapList.map(wap => ({
         ...wap,
         child: wap.child.map(child => ({
-          ...child,
-          startDate: child.startDate ? dayjs(child.startDate).format("YYYY-MM-DD") : undefined,
-          endDate: child.endDate ? dayjs(child.endDate).format("YYYY-MM-DD") : undefined,
+          waPeriodName:child.periodName,
+          waStartDate: child.startDate ? dayjs(child.startDate).format("YYYY-MM-DD") : undefined,
+          waEndDate: child.endDate ? dayjs(child.endDate).format("YYYY-MM-DD") : undefined,
         }))
       }))
     };
 
-    console.log(formattedData);
+   mutate(convertKeysToSnakeCase(formattedData),{
+         onSuccess: (data: any) => {
+           // Invalidate and refetch
+          console.log(data)
+           toast.success(data?.message);
+       
+           form.reset(); // Reset the form after successful submission
+            setListA(wapTypeOptions)
+         },
+         onError: (error) => {
+               showErrorToast(error?.response?.data.message);
+         },
+       })
   };
 
 
@@ -226,14 +218,17 @@ const District = () => {
   return <div className="text-lg font-medium text-slate-900 transition-colors dark:text-slate-50 ">
     <div className="flex w-full  h-[660px] justify-evenly items-center mt-2">
       <div className="flex flex-col gap-2 w-[45%] h-full border border-black rounded p-2  dark:border-slate-50">
-        <h1 className="h-6">List of A</h1>
+        <h1 className="h-6">List of Water accounting period types</h1>
+          { isWapTypeOptionsLoading ? <>loading water accounting period type </> :  
         <div className="flex flex-col gap-2 flex-grow overflow-y-auto ">
-          {listA.length > 0 && listA?.map((item: any) => <div key={item?.id} className="flex w-full gap-2 items-center h-12 bg-slate-400 px-2 rounded">
-            <input className="h-5 w-5" type="checkbox" onClick={() => !selectedA.includes(item?.id) ? setSelectedA([...selectedA, item?.id]) : setSelectedA((prev: any) => prev.filter((id: any) => id !== item?.id))} /> <div className="flex flex-grow text-xl">{item.name}</div>
+          
+      {listA?.length > 0 && listA?.map((item: any) => <div key={item?.value} className="flex w-full gap-2 items-center h-12 bg-slate-400 px-2 rounded">
+            <input className="h-5 w-5" type="checkbox" onClick={() => handleClick(selectedA,setSelectedA,item?.value) } /> <div className="flex flex-grow text-xl">{item?.label}</div>
           </div>)}
-        </div>
+        </div>}
+       
       </div>
-      <div className="w-[5%] flex flex-col items-center justify-center gap-2"><Button disabled={selectedA?.length === 0} onClick={handleRightShift}> <ArrowRight /></Button><Button disabled={selectedB?.length === 0} onClick={handleLeftShift}><ArrowLeft /> </Button></div>
+      <div className="w-[5%] flex flex-col items-center justify-center gap-2"><Button disabled={selectedA?.length === 0 || !form.watch("wayYear") } onClick={handleRightShift}> <ArrowRight /></Button><Button disabled={selectedB?.length === 0} onClick={handleLeftShift}><ArrowLeft /> </Button></div>
       <div className="flex flex-col gap-2 w-[45%] h-full border border-black rounded p-2  dark:border-slate-50">
         <Form {...form}>
           <form id="myForm" onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-2 flex-grow overflow-y-auto ">
@@ -243,23 +238,23 @@ const District = () => {
                 name='wayYear'
                 label='Water Accounting Year'
                 placeholder='Select Year'
-                options={yearList || []}
+                options={waysOptions?.data || []}
 
               />
             </div>
             {/* <div className="flex flex-col flex-grow overflow-y-auto g-2"> */}
-            <DndContext onDragEnd={reorderList} onDragStart={() => setOpenAccordion(fields.map((item: any) => item.wapId))}>
-              <SortableContext items={fields.map((item: any) => item?.wapId?.toString())}>
-                {fields.length > 0 && fields?.map((item: any, index) => <DragItemWrapper key={item.wapId} id={item.wapId.toString()}>
-                  <div className={cn("flex flex-col  w-full gap-2 items-center bg-slate-400 p-2 hover:bg-royalBlue rounded", openAccordion.includes(item.wapId) ? "h-auto" : "h-10")}>
+            <DndContext onDragEnd={reorderList} onDragStart={() => setOpenAccordion(fields.map((item: any) => item.waptId))}>
+              <SortableContext items={fields.map((item: any) => item?.waptId?.toString())}>
+                {fields.length > 0 && fields?.map((item: any, index) => <DragItemWrapper key={item.waptId} id={item.waptId.toString()}>
+                  <div className={cn("flex flex-col  w-full gap-2 items-center bg-slate-400 p-2 hover:bg-royalBlue rounded", openAccordion.includes(item.waptId) ? "h-auto" : "h-10")}>
                     <div className="flex w-full gap-2 items-center">
                       <input className="h-5 w-5" type="checkbox" onPointerDown={(e) => {
                         e.stopPropagation(); // important to prevent DnD taking over
-                        handleClick(selectedB, setSelectedB, item.wapId);
-                      }} /> <div className="flex flex-grow text-xl">{item.name}</div><div className="hover:bg-slate-200"><ChevronDown className={cn(openAccordion.includes(item.wapId) ? "rotate-180" : "")} onPointerDown={(e) => { e.stopPropagation(); handleAccordion(item?.wapId) }} /></div>
+                        handleClick(selectedB, setSelectedB, item.waptId);
+                      }} /> <div className="flex flex-grow text-xl">{item.name}</div><div className="hover:bg-slate-200"><ChevronDown className={cn(openAccordion.includes(item.waptId) ? "rotate-180" : "")} onPointerDown={(e) => { e.stopPropagation(); handleAccordion(item?.waptId) }} /></div>
                     </div>
-                    <div className={cn("flex justify-between items-center w-full g-1", openAccordion.includes(item.wapId) ? "" : "hidden")}>
-                      <NestedChildArray form={form} listIndex={index} parentName={item.name} control={form.control} />
+                    <div className={cn("flex justify-between items-center w-full g-1", openAccordion.includes(item.waptId) ? "" : "hidden")}>
+                      <NestedChildArray form={form} listIndex={index} parentName={item.name} control={form.control} yearList={waysOptions?.data || []}  />
                     </div>
                   </div>
                 </DragItemWrapper>)}
---
+++ src/services/timeSeries/index.ts
@@ -0,0 +1,25 @@
+import { useQuery, useMutation, UseQueryResult, useQueries } from "@tanstack/react-query";
+import { queryConfig } from "@/utils/reactQueryConfig";
+import { GET_WAPT_OPTIONS, GET_WAYS_OPTIONS, POST_WAYS } from "./constants";
+import { queryTimeSeries } from "./service";
+import { RegisterResponse } from "../registration/service";
+import { AxiosError } from "axios";
+
+export const useGetWaysOptions = ()=> {
+  return useQuery({
+    queryKey: [GET_WAYS_OPTIONS],
+    queryFn:()=> queryTimeSeries.getWaysList(),
+     ...queryConfig  });
+}
+export const useGetWaptOptions = ()=> {
+  return useQuery({
+    queryKey: [GET_WAPT_OPTIONS],
+    queryFn:()=> queryTimeSeries.getWaptList(),
+     ...queryConfig  });
+}
+
+export const usePostWays = () => {
+      return useMutation<RegisterResponse, AxiosError<any>, any>({
+        mutationKey: [POST_WAYS],
+        mutationFn: queryTimeSeries.postWays});
+}
