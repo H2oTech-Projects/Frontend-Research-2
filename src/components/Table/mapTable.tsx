278ddfe26cf3279ddc49df1fc3111a4147a63f67
jumirgosain <jumir@h2otechonline.com>
2025-06-23 15:22:09 +0545

    style: removed console

--- src/components/Table/mapTable.tsx
+++ src/components/Table/mapTable.tsx
@@ -270,7 +270,7 @@ const MapTable = <T,>({
                 {useClientPagination ?  <DataTablePagination table={table} collapse={collapse!}/> : <MapTablePagination totalData={totalData!} tableInfo={tableInfo!} setTableInfo={setTableInfo!} collapse={collapse!}/>}
             </div>
     }
-    console.log( customHeight ? `h-[${customHeight}]` : "h-auto")
+
     return (
       <div className="table-container flex flex-col overflow-hidden rounded-md bg-white shadow-md transition-colors dark:bg-slateLight-500">
         <div className={cn(fullHeight ? "h-[calc(100vh-208px)]" : customHeight ? `${customHeight}` : "h-auto")}>
