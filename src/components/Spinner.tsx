ca0879c3c6c86d88a734fb690d1f4f13d3ccfd34
jumirgosain <jumir@h2otechonline.com>
2025-08-24 13:57:52 +0545

    feat: [RT-87] added spiner color for dark theme

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import { Loader } from "lucide-react";
 const Spinner = () => {
   return (
     <div className="flex items-center justify-center">
-      <Loader className="w-6 h-6 animate-spin text-blue-500" />
+      <Loader className="w-6 h-6 animate-spin text-blue-500 dark:text-white-500" />
     </div>
   );
 };
