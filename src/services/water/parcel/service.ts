581b6d5491a01596d37232923abf7d6622041a45
jumirgosain <jumir@h2otechonline.com>
2025-09-02 12:36:36 +0545

    fix: [RT-87] removed catch in service file so that react query could catch error

--- src/services/water/parcel/service.ts
+++ src/services/water/parcel/service.ts
@@ -77,7 +77,7 @@ getSearchParcelMapByWAY : async (wapId:number, search: string) =>{
   const response = await axiosInstance.get(BASE_API_URL + "/ways/" + wapId + "/search_parcels/map/",{
     params:{
             search:search}
-    }).catch((err) => console.log(err));
+    })
   const data = convertKeysToCamelCase(toJson(response));
   return toJson(data?.data);
 },
