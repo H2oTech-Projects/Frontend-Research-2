8044430f20811a12990d7dc704f1104cdbb7c16b
jumirgosain <jumir@h2otechonline.com>
2025-09-04 12:26:35 +0545

    feat: {RT-96] Added  service file for crops

--- src/services/crops/index.ts
+++ src/services/crops/index.ts
@@ -1,6 +1,6 @@
 import { initialTableDataTypes } from "@/types/tableTypes";
 import { useMutation, useQuery, UseQueryResult } from "@tanstack/react-query";
-import { GET_ALL_CROP_FIELDS_LIST, GET_ALL_CROP_FIELDS_MAP, GET_BY_ID_CROP_FIELD, GET_CROPS_LIST, PUT_CROPS_FIELD } from "./constants";
+import { GET_ALL_CROP_FIELDS_LIST, GET_ALL_CROP_FIELDS_MAP, GET_BY_ID_CROP_FIELD, GET_CROPS_LIST, POST_CROPS, PUT_CROPS, PUT_CROPS_FIELD } from "./constants";
 import { queryCropsService } from "./services";
 import { noCacheQueryConfig, queryConfig } from "@/utils/reactQueryConfig";
 import { RegisterResponse } from "../registration/service";
@@ -11,6 +11,44 @@ export const useGetCropList = (tableInfo:initialTableDataTypes):UseQueryResult<a
     queryKey: [GET_CROPS_LIST,tableInfo?.page_no,tableInfo?.page_size,tableInfo?.search,tableInfo?.sort,tableInfo?.sort_order],
     queryFn: ()=> queryCropsService.getCropsList(tableInfo),
     ...queryConfig,
+  });
+}
+
+export const useGetCropById = (cropId:string):UseQueryResult<any> => {
+  return useQuery({
+    queryKey: [GET_CROPS_LIST,cropId],
+    queryFn: ()=> queryCropsService.getCropById(cropId),
+    enabled: !!cropId,
+    ...queryConfig,
+  });
+}
+
+export const useGetCropGroupNameDropdown = ():UseQueryResult<any> => {
+  return useQuery({
+    queryKey: [GET_CROPS_LIST,"cropGroupNameDropdown"], 
+    queryFn: ()=> queryCropsService.getCropGroupNameDropdown(),
+    ...noCacheQueryConfig,
+  });
+}
+
+export const usePostCrops = () => {
+  return useMutation<RegisterResponse, AxiosError<any>, any>({
+    mutationKey: [POST_CROPS],
+    mutationFn: queryCropsService.postCrops,
+  });
+}
+
+export const usePutCrops = () => {
+  return useMutation<RegisterResponse, AxiosError<any>, any>({
+    mutationKey: [PUT_CROPS],
+    mutationFn: queryCropsService.putCrops,
+  });
+}
+
+export const useDeleteCrops = () => {
+  return useMutation<RegisterResponse, AxiosError<any>, any>({
+    mutationKey: [PUT_CROPS],
+    mutationFn: queryCropsService.deleteCrops,
   });
 }
 
