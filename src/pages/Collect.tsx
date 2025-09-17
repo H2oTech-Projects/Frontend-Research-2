ccbe38c70184d4577798825737907e090a10014e
jumirgosain <jumir@h2otechonline.com>
2025-09-17 16:29:29 +0545

    style: [RT-107] updated button css and height of iframe

--- src/pages/Collect.tsx
+++ src/pages/Collect.tsx
@@ -1,29 +1,45 @@
-import React, { useState } from "react";
+import  { useState } from "react";
+import { RefreshCcw } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"; 
 
 const Collect = () => {
-  const [key, setKey] = useState(0); // Key forces iframe to re-render
-
+  const [key, setKey] = useState(0);
   const refreshIframe = () => {
     setKey((prev) => prev + 1); // Changing key refreshes the iframe
   };
 
   return (
     <div className="flex flex-col gap-2 px-3 py-2">
-      {/* Refresh Button */}
-      <button
-        onClick={refreshIframe}
-        className="self-end mb-2 rounded bg-blue-600 px-4 py-1 text-white hover:bg-blue-700 transition"
-      >
-        Refresh
-      </button>
+      <div className="relative w-full h-full">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button
+                onClick={refreshIframe}
+                className="absolute top-2 right-5 z-10 rounded bg-blue-600 p-2 text-white hover:bg-blue-700 transition"
+              >
+                <RefreshCcw size={16} />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent side="left" className="text-xs">
+              Refresh Form
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
-      {/* Iframe */}
-      <iframe
-        key={key} // Key triggers re-render when changed
-        src="https://fielddata.davidsengineering.com/-/single/WoAHUhCUUtVfCnQg5IwBoASaiFDyrpR?st=9JH7HgmxiYVi2wtuo!zsMO!7mk$9C3TDutMNPtezw8qBubAFDptgQEPqGT8gxA0Y"
-        title="Collect Page"
-        className="w-full h-[calc(100vh-124px)] rounded-xl border"
-      />
+        {/* Iframe */}
+        <iframe
+          key={key}
+          src="https://fielddata.davidsengineering.com/-/single/WoAHUhCUUtVfCnQg5IwBoASaiFDyrpR?st=9JH7HgmxiYVi2wtuo!zsMO!7mk$9C3TDutMNPtezw8qBubAFDptgQEPqGT8gxA0Y"
+          title="Collect Page"
+          className="w-full h-[calc(100vh-76px)] rounded-xl border"
+        />
+      </div>
     </div>
   );
 };
