6b2db8a05d077f423d8764436f2ecc36ee8fb72a
jumirgosain <jumir@h2otechonline.com>
2025-10-01 19:34:50 +0545

    refactor: [RT-108] hide zoom value in map

--- src/components/MapController.tsx
+++ src/components/MapController.tsx
@@ -22,7 +22,7 @@ const CustomZoomControl = () => {
 
   return (
     <div className="absolute bottom-11 right-4 z-[800] flex w-8 flex-col content-center justify-center gap-4 text-slate-50">
-      <div className="flex h-8 content-center justify-center rounded-lg bg-royalBlue p-2 text-sm font-bold">{zoomLevel}</div>
+      {/* <div className="flex h-8 content-center justify-center rounded-lg bg-royalBlue p-2 text-sm font-bold">{zoomLevel}</div> */}
       <div className="flex h-16 w-8 flex-col content-center justify-center rounded-lg bg-royalBlue">
         <button
           className="flex items-center justify-center border-b border-black border-opacity-50 pb-1 dark:border-white"
